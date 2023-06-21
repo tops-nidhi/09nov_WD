3741f3cc15ad9c91351fa13dc88c1b2823f872d7
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-06-21 10:46:21 +0530

    add

--- calc/min.js
+++ calc/min.js
@@ -5,23 +5,28 @@ for (i of buttons) {
     i.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
         console.log('Button text is ', buttonText);
-        if (buttonText == 'X') {
+        if(buttonText == 'X')
+        {
             buttonText = '*';
-            screenValue += buttonText; //screenValue = screenValue + buttonText
-            result.value += screenValue;
-        }
-        
-        else if (buttonText == 'C') {
-            screenValue = "";
-            result.value = screenValue;
-        }
-        else if (buttonText == '=') {
-            result.value = eval(screenValue);
-        }
-        else {
             screenValue += buttonText;
             result.value = screenValue;
         }
-        
+        else if(buttonText == 'C')
+        {
+            screenValue = '';
+            result.value = screenValue ;
+            // console.log("Clear");
+        }
+        else if(buttonText == '=')
+        {
+            result.value = eval(screenValue);
+            screenValue = result.value;
+            result.value = screenValue;
+        }
+        else
+        {
+            screenValue += buttonText;
+            result.value = screenValue;
+        }
     })
 }
